be8655cb98ce483b08781768cd1e6eca13395a6c
SiddarthaBeemaneni <siddarthabeemaneni@gmail.com>
2026-09-04 11:10:31 +0530

    Initial commit

---
+++ src/Encapsulation.java
@@ -0,0 +1,27 @@
+class Human{
+    private int age;
+    private String name;
+    public int getAge(){
+        return age;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(int a) {
+        age = a;
+    }
+public void setName(String n) {
+        name = n;
+}
+}
+
+
+        public class Encapsulation {
+          static void main(String[] args) {
+        Human obj = new Human();
+        obj.setAge(18);
+        obj.setName("Siddartha");
+        System.out.println("Name :" + obj.getName() + "Age : " + obj.getAge());
+    }
+}
